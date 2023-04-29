81af44e4f73bb6b9e78b9f5284387fe79d54ef03
Matti Eiden <snaipperi@gmail.com>
2023-04-29 23:08:06 +0300

    Update bug.py

--- bug.py
+++ bug.py
@@ -1,14 +1,4 @@
-def show(show_object=None):
-    if show_object is None:
-        import __main__
-
-        show_object = __main__.__dict__.get("show_object")
-
-    if show_object is None:
-        raise ValueError(
-            "Unable to visualize job, no show_object provided or found"
-        )
-
+def show(show_object):
     match source_module := show_object.__module__.split(".")[0]:
         case "cq_editor":
             pass
