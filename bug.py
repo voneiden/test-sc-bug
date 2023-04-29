b9f229a3c83f477276df175b320fd5f2c1bad082
Matti Eiden <snaipperi@gmail.com>
2023-04-29 23:03:05 +0300

    Update bug.py

--- bug.py
+++ bug.py
@@ -1,5 +1,21 @@
-match x := 'zomg':
-    case _:
-        print(
-            f"x is {x}"
+def show(show_object=None):
+    if show_object is None:
+        import __main__
+
+        show_object = __main__.__dict__.get("show_object")
+
+    if show_object is None:
+        raise ValueError(
+            "Unable to visualize job, no show_object provided or found"
         )
+
+    match source_module := show_object.__module__.split(".")[0]:
+        case "cq_editor":
+            visualize_f = 2
+        case "ocp_vscode":
+            visualize_f = 3
+        case _:
+            logger.warning(
+                f"Unsupported show_object source module ({source_module}) - visualizing as edges"
+            )
+            visualize_f = 3
