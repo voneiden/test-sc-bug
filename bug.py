97a0532e7b1e765bb139819777245bca503edb33
Matti Eiden <snaipperi@gmail.com>
2023-04-29 23:18:35 +0300

    Update bug.py

--- bug.py
+++ bug.py
@@ -1,6 +1,6 @@
-def foo(bar):
-    match x := "foo":
-        case "cq_editor":
-            print("ok")
-        case _:
-            print(x)
+
+match x := "foo":
+    case "cq_editor":
+        print("ok")
+    case _:
+        print(x)
