cf6eaa24021b53235ed63fa94ea627b0145eacf8
Matti Eiden <snaipperi@gmail.com>
2023-04-29 23:08:54 +0300

    Update bug.py

--- bug.py
+++ bug.py
@@ -1,9 +1,7 @@
 def show(show_object):
     match source_module := show_object.__module__.split(".")[0]:
         case "cq_editor":
-            pass
-        case "ocp_vscode":
-            pass
+            print("ok")
         case _:
             logger.warning(
                 f"Unsupported show_object source module ({source_module}) - visualizing as edges"
